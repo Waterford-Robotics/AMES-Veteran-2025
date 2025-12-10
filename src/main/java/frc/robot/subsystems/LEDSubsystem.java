c8a6fe4b55928804f98e156b9a7e63b883132e69
Nitya Sunkara <nityasunkara@students.waterfordschool.org>
2025-12-10 16:00:07 -0700

    created LED subsystem

---
+++ src/main/java/frc/robot/subsystems/LEDSubsystem.java
@@ -0,0 +1,79 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class LEDSubsystem extends SubsystemBase {
+   Spark m_blinkin;
+
+    public LEDSubsystem() {
+        //enter port constant
+        m_blinkin = new Spark(0);
+    }
+    
+    
+    //colors
+
+    public void setRed() {
+        m_blinkin.set(0.61);
+    }
+
+    public void setOrange() {
+        m_blinkin.set(0.65);
+    }
+
+    public void setYellow() {
+        m_blinkin.set(0.69);
+    }
+
+    public void setGreen() {
+        m_blinkin.set(0.77);
+    }
+
+    public void setBlue() {
+        m_blinkin.set(0.87);
+    }
+
+    public void setPurple() {
+        m_blinkin.set(0.91);
+    }
+
+    public void setTwinklesOcean() {
+        m_blinkin.set(-0.51);
+    }
+
+    public void turnOff() {
+        m_blinkin.set(0.00);
+    }
+
+
+    //need color sensor subsystem stuffs
+
+    public void periodic() {
+       String color = SmartDashboard.getString("DetectedColor", "None");
+       
+       if (color.equals("Red")) {
+        setRed();
+       } 
+       else if (color.equals("Orange")) {
+        setOrange();
+       } 
+       else if (color.equals("Yellow")) {
+        setYellow();
+       }
+       else if (color.equals("Green")) {
+        setGreen();
+       }
+       else if (color.equals("Blue")) {
+        setBlue();
+       }
+       else if (color.equals("Purple")) {
+        setPurple();
+       }
+       else {
+        turnOff();
+       }
+
+    }
+}
